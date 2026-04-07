d4afd02e2de603f636874677f5e9e76ab3694af4
Johanes Theo Adrian <johanestheo07@gmail.com>
2026-04-07 13:50:01 +0700

    asu

--- js/landing.js
+++ js/landing.js
@@ -1,10 +1,10 @@
 const btnai = document.getElementById("chat-ai");
 const btnfr = document.getElementById("forum-chat");
 
-btnai.addEventListener('click', () => {
-    window.location.href = '../pages/chatai.html';
+btnai.addEventListener("click", () => {
+    window.location.href = "../pages/chatai.html";
 });
 
-btnfr.addEventListener('click', () => {
-    window.location.href = '../pages/forum.html';
-});
+btnfr.addEventListener("click", () => {
+    window.location.href = "../pages/forum.html";
+});   
