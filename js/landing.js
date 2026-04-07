0a82a03de3f458ba376a970c871e93dcf51550bf
Johanes Theo Adrian <johanestheo07@gmail.com>
2026-04-07 11:07:20 +0700

    su

--- js/landing.js
+++ js/landing.js
@@ -3,8 +3,8 @@ const btnfr = document.getElementById("forum-chat");
 
 btnai.addEventListener('click', () => {
     window.location.href = '../pages/chatai.html';
-}
+});
 
 btnai.addEventListener('click', () => {
     window.location.href = '../pages/forum.html';
-}
+});
